7edc800346ae01304a020cf33b587193560ef4cb
Marco Molin <marcomolin45@gmail.com>
2019-11-05 12:26:58 -0300

    cambios en el controller de biddings

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -1,2 +1,23 @@
 class BiddingsController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def category
+  end
+
+  def brands
+  end
+
+  def location
+  end
 end
--- app/models/bidding.rb
+++ app/models/bidding.rb
@@ -1,5 +1,6 @@
 class Bidding < ApplicationRecord
   belongs_to :user
+  has_many :brandRanks
   validates :amount, presence: true
   validates :category, presence: true
 end
