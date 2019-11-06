37a77d434a216515ebf24dbd75a1b12182092658
Paula Franichevich <pfranichevich94@gmail.com>
2019-11-06 20:07:41 -0300

    front del home en proceso

--- db/schema.rb
+++ db/schema.rb
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2019_11_05_222104) do
   end
 
   create_table "brand_ranks", force: :cascade do |t|
-    t.integer "order"
     t.bigint "brand_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
