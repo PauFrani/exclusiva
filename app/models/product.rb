3e5ec37950995d214c0e5b20782a3a7ad52865b3
Linda Mollinedo <lindamse@gmail.com>
2019-11-07 12:54:04 -0300

    update

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,7 +10,10 @@ class Product < ApplicationRecord
 
   def self.search(bidding)
     sql_query = "SELECT * FROM products as pr JOIN product_items AS pi ON pr.id = pi.product_id \
-                JOIN showrooms AS sh ON pi.showroom_id = "
-    #where(sql_query, query: "%#{query}%")
+                JOIN showrooms AS sr ON pi.showroom_id = sr.id \
+                JOIN brands AS b ON b.id = sr.brand_id \
+                JOIN brand_ranks AS br ON b.id = br.brand_id \
+                where p.category = bidding.category and bidding.amount between p.min_price and p.max_price"
+    Product.find_by_sql(sql_query)
   end
 end
