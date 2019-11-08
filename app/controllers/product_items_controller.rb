d0d3d74a6b5bf15ebfef333c56b7d6c3c4f8d7cd
Oscar Rodas <oscar.d.rodas@gmail.com>
2019-11-08 10:51:39 -0300

    results 90porciento

--- app/controllers/product_items_controller.rb
+++ app/controllers/product_items_controller.rb
@@ -4,13 +4,16 @@ class ProductItemsController < ApplicationController
   def index
     @bidding = Bidding.find(session[:bidding_id])
     @products_items = ProductItem.joins(:product).where(products: { category: @bidding.category }).where("products.min_price < ?", @bidding.amount).where("products.max_price > ?", @bidding.amount).where("product_items.stock > ?", 0)
-    @geocodes = @products_items.map do |product| product.showroom.address end
-    @markers = @geocodes.map do |product|
-      if product.latitude != nil
-      {
-        lat: product.latitude,
-        lng: product.longitude
-      }
+    @markers = @products_items.map do |product_item|
+      if product_item.showroom.address.latitude != nil
+        {
+          lat: product_item.showroom.address.latitude,
+          lng: product_item.showroom.address.longitude,
+          infoWindow: render_to_string(partial: "info_window", locals: { product_item: product_item }),
+          image_url: helpers.asset_url('https://i.ibb.co/MVHrLcB/perchero2.png')
+          # image_url: helpers.asset_url('https://i.ibb.co/zfL4Rt9/perchero3.png')
+
+        }
       end
     end
   end
@@ -19,3 +22,18 @@ class ProductItemsController < ApplicationController
     @product_item = ProductItem.find(params[:id])
   end
 end
+
+  # def index
+  #   @bidding = Bidding.find(session[:bidding_id])
+  #   @products_items = ProductItem.joins(:product).where(products: { category: @bidding.category }).where("products.min_price < ?", @bidding.amount).where("products.max_price > ?", @bidding.amount).where("product_items.stock > ?", 0)
+  #   @product_addresses = @products_items.map { |product_item| product_item.showroom.address }
+  #   @markers = @product_addresses.map do |address|
+  #     if address.latitude.nil?
+  #       {
+  #         lat: address.latitude,
+  #         lng: address.longitude,
+  #         infoWindow: render_to_string(partial: "info_window", locals: { product_item:  })
+  #       }
+  #     end
+  #   end
+  # end
