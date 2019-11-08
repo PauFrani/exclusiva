a73907c0de2b53f6409d5e12d3615ea7be79f9fa
Paula Franichevich <pfranichevich94@gmail.com>
2019-11-08 16:40:08 -0300

    select category funcionando

---
+++ app/javascript/plugins/select_category.js
@@ -0,0 +1,31 @@
+const cards = document.querySelectorAll('.card-bidding-category');
+
+const initCategorySelector = () => {
+
+  if (cards) {
+    console.log(cards)
+    cards.forEach( card => card.addEventListener('click', selectCategory));
+  }
+
+};
+
+const selectCategory = (event) => {
+
+  const card = event.currentTarget;
+
+  // remove active card
+  const activesCards = document.querySelectorAll('.card-bidding-category.active')
+  activesCards.forEach( card => card.classList.remove('active') )
+
+  // populate input hidden
+  console.log(card.dataset.category);
+  const input = document.getElementById('bidding_category');
+
+  input.value = card.dataset.category;
+
+  // add active to selected card
+  card.classList.add('active')
+
+}
+
+export { initCategorySelector };
