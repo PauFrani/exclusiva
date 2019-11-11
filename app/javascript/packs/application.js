258c4edc265cc09f5914fbd9f559e76582aeceae
Oscar Rodas <oscar.d.rodas@gmail.com>
2019-11-11 18:57:52 -0300

    images select in show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,8 +5,8 @@ import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initSlick } from '../plugins/init_slick';
 import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initAutocomplete1 } from '../plugins/init_autocomplete';
-import { initCategorySelector } from '../plugins/select_category'
-import { initImages } from '../plugins/init_thumbnails'
+import { initCategorySelector } from '../plugins/select_category';
+import { initImages } from '../plugins/init_images';
 
 initMapbox();
 initAutocomplete();
