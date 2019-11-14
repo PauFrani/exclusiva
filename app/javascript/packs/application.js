c8901c292ed4d066877442b34477d546b6e035a3
Linda Mollinedo <lindamse@gmail.com>
2019-11-14 16:51:10 -0300

    fix algolia

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initSlick } from '../plugins/init_slick';
-import { initAutocomplete, initAutocomplete1 } from '../plugins/init_autocomplete';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 import { initCategorySelector } from '../plugins/select_category';
 import { initSortable } from '../plugins/init_sortable';
 import { initImages } from '../plugins/init_images'
@@ -14,7 +14,6 @@ import { launchTimer } from '../plugins/init_timer'
 
 initMapbox();
 initAutocomplete();
-initAutocomplete1();
 initCategorySelector();
 initSortable();
 initImages();
