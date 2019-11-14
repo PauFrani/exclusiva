a439cc9d00b4db630d96226304dc3e715b93d0a3
Linda Mollinedo <lindamse@gmail.com>
2019-11-14 17:20:46 -0300

    fixed algolia placejs keys

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -3,9 +3,12 @@ import places from 'places.js';
 const initAutocomplete = () => {
   const addressInput = document.getElementById('bidding_address_street');
   if (addressInput) {
-    appId: 'plS27C4JI3TY',
-    apiKey: '0975179f03fc65cd0070b624e9253e9c',
-    places({ container: addressInput, countries: ["AR"]  });
+    places({
+      container: addressInput,
+      countries: ["AR"],
+      appId: 'plS27C4JI3TY',
+      apiKey: '0975179f03fc65cd0070b624e9253e9c',
+    });
   }
 };
 
