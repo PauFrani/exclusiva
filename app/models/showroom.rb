46de95ca5367bca519968e74bd9a3c98384821c3
Oscar Rodas <oscar.d.rodas@gmail.com>
2019-11-21 22:20:56 -0300

    brands category show correct categories

--- app/models/showroom.rb
+++ app/models/showroom.rb
@@ -2,6 +2,7 @@ class Showroom < ApplicationRecord
   belongs_to :brand
   has_many :showroom_variant_stocks
   has_many :purchases, through: :showroom_variant_stocks
+   has_many :variants, through: :showroom_variant_stocks
   validates :name, presence: true
   validates :phone_number, presence: true
   validates :email, presence: true
